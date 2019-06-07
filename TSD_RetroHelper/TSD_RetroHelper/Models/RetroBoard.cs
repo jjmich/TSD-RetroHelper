b950dabefd2d5852a5903c4f1eb4d38e893775d2
Kuba <kuba_mich@outlook.com>
2019-06-07 12:46:58 +0200

    model for RetroBoard

---
+++ TSD_RetroHelper/TSD_RetroHelper/Models/RetroBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TSD_RetroHelper.Models
+{
+    public class RetroBoard
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        List<RetroItem> WentWell { get; set; }
+        List<RetroItem> ToImprove { get; set; }
+        List<RetroItem> ActionItems { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime RetroDate { get; set; } 
+
+    }
+}
