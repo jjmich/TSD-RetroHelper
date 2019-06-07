0cbcbce001179150c415d6a1bc62a6d31609a35a
Kuba <kuba_mich@outlook.com>
2019-06-07 12:15:34 +0200

    RetroItem model

---
+++ TSD_RetroHelper/TSD_RetroHelper/Models/RetroItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TSD_RetroHelper.Models
+{
+    public class RetroItem
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
